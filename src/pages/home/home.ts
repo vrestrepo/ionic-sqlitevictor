82c3590a261d601496b119b8b5561039975870f6
victorc753 <victorc753@gmail.com>
2017-11-10 15:54:36 -0500

    pry

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { SQLite, SQLiteObject } from '@ionic-native/sqlite';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +8,27 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  private database: SQLiteObject;
+  personas : any;
 
+  constructor(public navCtrl: NavController,  private sqlite: SQLite) {
+    this.personas = [];
+    this.sqlite.create({
+      name: 'data2.db',
+      location: 'default'
+    })
+      .then((db: SQLiteObject) => {
+        this.database = db;
+        this.database.executeSql('create table IF NOT EXISTS usuarios(nombre TEXT,telefono TEXT)', {})
+          .then(() => console.log('Executed SQL'))
+          .catch(e => console.log(e));
+      })
+      .catch(e => console.log(e));
   }
-
+  insertar(nom,tel){
+    this.database.executeSql(`INSERT INTO usuarios(nombre,telefono) VALUES ('${nom}','${tel}');`, {}).then((result)=>{
+      this.personas.push(nom + " "+tel);
+    })
+    .catch(e => console.log(e));
+  }
 }
